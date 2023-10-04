86a0953fb21c859ededfc24253e6bf808f64fdf3
U-DESKTOP-2GEDOLC\gjche <gjchen2005@gmail.com>
2023-10-03 21:06:10 -0700

    TicTacToe Finished

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -2,6 +2,8 @@
 //Gordon Chen
 //September 29th, 2023
 
+//Note: Doesn't check for some edgecases like if an input is not one that corresponds to the 3x3 board
+
 #include <iostream>
 #include <cstring>
 
@@ -20,6 +22,7 @@ int main()
   int k = 0;
   bool game_end = false;
   bool player_1 = false;
+  bool win = false;
   int player_1_wins = 0;
   int player_2_wins = 0;
   
@@ -29,17 +32,17 @@ int main()
     char player_symbol = ' ';
 
     if(k%2==0){
-	cout << "Player 1 (X): Input your row letter" << endl;
+	cout << "Player 1 (X): Input your row letter (a, b, or c)" << endl;
 	cin >> temp_i;
-	cout << "Player 1 (X): Input your column number" << endl;
+	cout << "Player 1 (X): Input your column number (1, 2, or 3)" << endl;
 	cin >> temp_j;
 	player_symbol = 'X';
 	player_1 = true;
       }
       else {
-	cout << "Player 2 (O): Input your row letter" << endl;
+	cout << "Player 2 (O): Input your row letter (a, b, or c)" << endl;
 	cin >> temp_i;
-	cout << "Player 2 (O): Input your column number" << endl;
+	cout << "Player 2 (O): Input your column number (1, 2, or 3)" << endl;
 	cin >> temp_j;
 	player_symbol = 'O';
 	player_1 = false;
@@ -69,13 +72,10 @@ int main()
 
     cout << "Current Board: ";
 
-    int filled = 0;
-
     for(i=0; i<4; i++){
       cout << "\n";
       for(j=0; j<4; j++){
 	cout<< "\t"<<board[i][j];
-	filled++;
       }
     }
 
@@ -85,65 +85,67 @@ int main()
     //checks for row wins
     
     j = 1;
-    i = 1;
     for(i=1; i<4; i++){
-      if(board[i][j] == board[i][j+1] && board[i][j] == board[i][j+2]){
+      if(board[i][j] == board[i][j+1] && board[i][j] == board[i][j+2] && board[i][j] != ' '){
 	game_end = true;
       }
     }
+
+    i = 1;
     //checks for column wins
     for(j=1; j<4; j++){
-      if(board[i][j] == board[i+1][j] board[i][j] == board[i+2][j]{
+      if(board[i][j] == board[i+1][j] && board[i][j] == board[i+2][j] && board[i][j] != ' '){
 	  game_end = true;
 	}
     }
 
       //checks for diagonal wins
-      if (board[1][1] == board[2][2] && board[1][1] == board[3][3]){
+      if (board[1][1] == board[2][2] && board[1][1] == board[3][3] && board[1][1] != ' '){
 	game_end = true;
       }
-      if (board[3][1] == board[2][2] && board[3][1] == board[1][3]{
+      if (board[3][1] == board[2][2] && board[3][1] == board[1][3] && board[3][1] != ' '){
 	  game_end = true;
 	}
 
-	if(game_end = true){
+	if(game_end == true){
 	  if(player_1 == true){
 	    player_1_wins++;
-	    cout << "Player 1 wins!" << endl;
+	    cout << "\nPlayer 1 wins!" << endl;
 	  }
 	  else{
 	    player_2_wins++;
-	    cout << "Player 2 wins!" << endl;
+	    cout << "\nPlayer 2 wins!" << endl;
 	  }
 	  
 	}
 
-    if(filled == 9 && game_end == false){
-      cout << "The board is filled! The game ends in a draw" << endl;
-    }
-    else{
-      filled = 0;
+    if(k == 9 && game_end == false){
+      cout << "\nThe board is filled! The game ends in a draw" << endl;
+      game_end = true;
     }
 
-	cout << "Current win totals: \n" << "Player 1: " << player_1_wins << '\n' << "Player 2: " << player_2_wins << endl;
-	char again;
-	cout<<"Play again? (y/n)"<<endl;
-	cin >> again;
-	if (again == 'y'){
-	    game_end = false;
-	     board[={ //first num refers to the row, second refers to the column
-		 {' ','1','2','3'},
-		 {'a',' ',' ',' '},
-		 {'b',' ',' ',' '},
-		 {'c',' ',' ',' '}
-
+    if(game_end == true){
+      cout << "\nCurrent win totals: \n" << "Player 1: " << player_1_wins << '\n' << "Player 2: " << player_2_wins << endl;
+      char again;
+      cout<<"\nPlay again? (y/n)"<<endl;
+      cin >> again;
+      if (again == 'y'){
+	game_end = false;
+	k=0;
+	for(i=1; i<4; i++){  //clear board
+	  for(j=1; j<4; j++){
+	    board[i][j] = ' ';
 	  }
+	}
+      }
+      else{
+	cout << "\nThank you for playing!" << endl;
+      }
 
-    
+    }
+
     cout << endl;
-
   }
-
-  
+ 
   return 0;
-}
+  }
