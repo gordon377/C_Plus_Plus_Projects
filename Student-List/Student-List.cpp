ad97b704fb90c8f7e989c64c84064700a1b7929a
U-BSD\412999 <412999@SHS-F4W2NW3.beaverton.k12.or.us>
2023-10-16 10:49:49 -0700

    Student List Progress

--- Student-List/Student-List.cpp
+++ Student-List/Student-List.cpp
@@ -3,6 +3,8 @@
 //October 11th, 2023
 
 #include <iostream>
+#include <cstring>
+#include <vector>
 
 using namespace std;
 
@@ -15,12 +17,47 @@ struct Student {
 
 };
 
-
+void ADD(vector <Student*> &svect);
+void PRINT(svect);
+void DELETE(svect);
+void QUIT();
 
 int main(){
 
-  char input[7]; //Function choice input
+  vector <Student*> svect; //Initialize vector of pointers in the form of the Student structure
 
+  char input[20]; //Function choice input
+  cout << "Type ADD, PRINT, DELETE, or QUIT (case sensitive) to interact with this student list" << endl;
+  cin >> input;
+  if 
   
   return 0;
+}
+
+void ADD(vector <Student*> &svect) {
+
+  Student* s = new Student();
+  cout << "Enter the Student's first name: " << endl;
+  cin >> s->first_name; //Same as (*s).first_name | Dots are first in C++ order of operations
+  cout << "Enter the Student's last name: " << endl;
+  cin >> s->last_name;
+  cout << "Enter the Student's ID: " << endl;
+  cin >> s->id;
+  cout << "Enter the Student's GPA: " << endl;
+  cin >> s->gpa;
+
+  svect.pushback(s);
+
+}
+
+void PRINT(svect) {
+
+}
+
+void DELETE(svect) {
+
+}
+
+void QUIT() {
+
 }
