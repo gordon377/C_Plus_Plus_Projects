a1e9daf3395d75de0670dd51bac817defbc745aa
U-BSD\412999 <412999@SHS-F4W2NW3.beaverton.k12.or.us>
2023-10-16 09:40:21 -0700

    Initial Student List Commit

---
+++ Student-List/Student-List.cpp
@@ -0,0 +1,26 @@
+//Student List
+//Gordon Chen
+//October 11th, 2023
+
+#include <iostream>
+
+using namespace std;
+
+struct Student {
+
+  char first_name[81];
+  char last_name[81];
+  int id;
+  float gpa;
+
+};
+
+
+
+int main(){
+
+  char input[7]; //Function choice input
+
+  
+  return 0;
+}
