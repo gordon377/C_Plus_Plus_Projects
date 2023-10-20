6f097b557f187ae7e511574670ba0dff451ec5f6
U-DESKTOP-2GEDOLC\gjche <gjchen2005@gmail.com>
2023-10-19 23:32:12 -0700

    Student List Finished

--- Student-List/Student-List.cpp
+++ Student-List/Student-List.cpp
@@ -5,10 +5,11 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
-struct Student {
+struct Student { //Define Student structure
 
   char first_name[81];
   char last_name[81];
@@ -17,56 +18,86 @@ struct Student {
 
 };
 
+//function prototypes
+
 void ADD(vector <Student*> &svect);
 void PRINT(vector <Student*> &svect);
 void DELETE(vector <Student*> &svect);
-void QUIT();
 
 int main(){
 
   vector <Student*> svect; //Initialize vector of pointers in the form of the Student structure
 
   char input[20]; //Function choice input
-  cout << "Type ADD, PRINT, DELETE, or QUIT (case sensitive) to interact with this student list" << endl;
-  cin >> input;
-  if(strcmp(input, "ADD") == 0){
-    ADD(vector <Student*> &svect);
-  } 
+  bool end = false; //Continue condition
+  while(end == false){ //Loops while end == false
+    
+    cout << "Type ADD, PRINT, DELETE, or QUIT (case sensitive) to interact with this student list" << endl;
+    cin >> input; //Take in command
+    if(strcmp(input, "ADD") == 0){  //Calling function corresponding to requested command
+      ADD(svect);
+    }
+    else if(strcmp(input, "PRINT") == 0){
+      PRINT(svect);
+    }
+    else if(strcmp(input, "DELETE") == 0){
+      DELETE(svect);
+    }
+    else if(strcmp(input, "QUIT") ==0){
+	end = true;
+      }
+    else{
+
+      cout << "Invalid input (case sensisitve commands)" << endl;  //Other case
+      
+    }
+  
+  }
   
   return 0;
 }
 
-void ADD(vector <Student*> &svect) {
+void ADD(vector <Student*> &svect) { //Adding new student with info
 
-  Student* s = new Student();
+  Student* s = new Student(); //Create new variable of student structure (new is called to keep the information for longer/on the heap)
   cout << "Enter the Student's first name: " << endl;
   cin >> s->first_name; //Same as (*s).first_name | Dots are first in C++ order of operations
   cout << "Enter the Student's last name: " << endl;
   cin >> s->last_name;
-  cout << "Enter the Student's ID: " << endl;
+  cout << "Enter the Student's ID: " << endl; //Taking in student info
   cin >> s->id;
   cout << "Enter the Student's GPA: " << endl;
   cin >> s->gpa;
 
-  svect.pushback(s);
+  svect.push_back(s); //Pushing information back to vector
 
 }
 
-void PRINT(vector <Student*> &svect) {
+void PRINT(vector <Student*> &svect) { //Printing stored student data
 
-  vector<int>::iterator ptr;
-  for(ptr = vect.begin(); ptr < vect.end(); ptr++){
+  vector<Student*>::iterator ptr = svect.begin(); //initialize iterator | (iterator returns pointers) | "ptr" (variable name) is a ptr to a specific value in the vector (in this case points to a specific student list ptr in the vector)
+  for(ptr; ptr < svect.end(); ptr++){
     cout  << "Name: " << (*ptr)->first_name << " " << (*ptr)->last_name << endl;
     cout << "ID: " << (*ptr)->id << endl;
-    cout << "GPA: " << fixed << setprecision(2) << (*ptr)->gpa << endl;
+    cout << "GPA: " << fixed << setprecision(2) << (*ptr)->gpa << endl; //Precisiion fixed to two decimals
   }
   
 }
 
 void DELETE(vector <Student*> &svect) {
+  
+  float id_input;
+  cout << "Enter the ID of the student to delete" << endl;
+  cin >> id_input;
+  for(vector<Student*>::iterator ptr = svect.begin(); ptr != svect.end(); ptr++){
 
+    if (id_input == (*ptr)->id){
+
+	delete *ptr; //delete student list pointer within vector
+	svect.erase(ptr); //delete iterator ptr memory
+	break; //leave loop
+      }
+
+  }
 }
 
-void QUIT() {
-
-}
