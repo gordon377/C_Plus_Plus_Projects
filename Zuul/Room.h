0d423906aebe62067575c98c28125fb411c22956
U-BSD\412999 <412999@SHS-F4W2NW3.beaverton.k12.or.us>
2023-11-17 10:42:56 -0800

    Initial Zuul Commit

---
+++ Zuul/Room.h
@@ -0,0 +1,21 @@
+//Header Guard Protecction
+
+#ifndef ROOM
+#define ROOM
+
+#include <iostream>
+
+using namespace std;
+
+class Room{
+
+ public:
+  //Items (Room Inventory)
+  //Exits
+  //Description of room (flavor)
+  bool trapped = false; //extra condition for making rooms trapped (can lose game)
+
+  // protected: (don't think this is needed)
+  
+
+};
---
+++ Zuul/main.cpp
@@ -0,0 +1,17 @@
+//November 17th, 2023
+//Gordon Chen
+//Zuul
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+//prototypes here
+
+int main(){ //main function
+
+
+
+}
