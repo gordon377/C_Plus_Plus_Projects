99c4ebb71bcbc65bf88144b643c9a33da02f7c80
U-DESKTOP-2GEDOLC\gjche <gjchen2005@gmail.com>
2024-03-15 00:14:58 -0700

    Node Class rough outline

--- ShuntingYard/node.cpp
+++ ShuntingYard/node.cpp
@@ -14,3 +14,41 @@ Node::Node() {
 Node::~Node(){
   
 }
+
+
+Node* Node::returnRight(){
+  return right;
+}
+
+Node* Node::returnLeft(){
+  return left;
+}
+
+char* Node::returnValue(){
+  return value;
+}
+
+Node* Node::returnNext(){
+  return next;
+}
+
+
+
+
+
+void Node::makeRight(Node* inputRight){
+  right = inputRight;
+}
+
+void Node::makeLeft(Node* inputLeft){
+  left = inputLeft;
+}
+
+void Node::makeValue(char* inputValue){
+  value = inputValue;
+}
+
+void Node::makeNext(Node* inputNext){
+  next = inputNext;
+}
+
