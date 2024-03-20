a0d6cfd1f3c7168b313f326615457e8847c50a9c
U-BSD\412999 <412999@SHS-B4W2NW3.beaverton.k12.or.us>
2024-03-20 10:50:12 -0700

    Working on functions...

--- Binary_Search_Tree/main.cpp
+++ Binary_Search_Tree/main.cpp
@@ -15,7 +15,7 @@ void SORT_INPUT(Node* inputNode, Node* currNode); //Input Value into tree proper
 void PRINT();
 void REMOVENUM(); //To remove a nodes of a certain num
 void ADDNUM(int inputNum);
-bool SEARCH();
+bool SEARCH(Node* currNode, int inputNum);
 
 
 
@@ -85,7 +85,9 @@ int main(){
       ADDNUM(inputValue);
     }
     else if(strcmp, (input, "SEARCH") == 0){
-      SEARCH(); //Figure out SEARCH function...
+      cout << "What number to check for?" << endl;
+      cin >> inputValue;
+      SEARCH(rootNode, inputValue); //Figure out SEARCH function...
     }
     else if(strcmp(input, "QUIT") == 0){
       active = false;
@@ -100,45 +102,116 @@ int main(){
 }
 
 void SORT_INPUT(Node* inputNode, Node* currNode){ //Top Down Sort (From Root)
-  if(inputNode->returnValue = currNode->returnValue){ //Equal Case (Sorts Right | Right-Leaning Bias)
+  if(inputNode->returnValue() = currNode->returnValue()){ //Equal Case (Sorts Right | Right-Leaning Bias)
     currNode->makeRight(inputNode);
   }
-  else if(inputNode->returnValue > currNode->returnValue){ //Greater Case (Check Right)
-    if(currNode->returnRight == NULL){
+  else if(inputNode->returnValue() > currNode->returnValue()){ //Greater Case (Check Right)
+    if(currNode->returnRight() == NULL){
       currNode->makeRight(inputNode);
     }
     else{
-      currNode = currNode->returnRight;
+      currNode = currNode->returnRight();
       SORT_INPUT(inputNode, currNode);
     }
   }
-  else if(inputNode->returnValue < currNode->returnValue){ //Less than Case (Check Left)
+  else if(inputNode->returnValue() < currNode->returnValue()){ //Less than Case (Check Left)
     if(currNode->returnLeft == NULL){
       currNode->makeLeft(inputNode)
     }
     else{
-      currNode = currNode->returnRight;
+      currNode = currNode->returnRight();
       SORT_INPUT(inputNode, currNode);
     }
   }
   return;
 }
 
-void ADDNUM(int inputNum){
+void ADDNUM(int inputNum){ //Add number to binary tree
   Node* tempNode = new Node();
   tempNode->makeValue(inputNum);
   SORT_INPUT(tempNode, rootNode);
   return;
 }
 
-void PRINT(){
+void PRINT(){ //Visually print binary tree
   return;
 }
 
-void SEARCH(){
-  return;
+void SEARCH(Node* currNode, int inputNum){ //Check if number is in binary tree
+  if(currNode->returnValue() == inputNum){ //Matching Case
+    cout << "That number is in the binary tree" << endl;
+    return;
+  }
+  else if(currNode->returnValue() < inputNum){ //Input is greater than current node value
+    if(currNode->returnRight() == NULL){ //No match
+      cout << "That number is not in the binary tree" << endl;
+      return;
+    }
+    else(){ //Go right and continue searching
+	currNode = currNode->returnRight();
+	SEARCH(currNode, inputNum);
+      }
+  }
+  else if(currNode->returnValue() > inputNum){ //Input is less than current node value
+    if(currNode->returnLeft() == NULL){
+      cout << "That number is not in the binary tree" << endl;
+      return;
+    }
+    else(){ //Go left and continue searching
+	currNode = currNode->returnLeft();
+	SEARCH(currNode, inputNum);
+      }
+  }
 }
 
-void REMOVENUM(){
-  return;
+void REMOVENUM(Node* currNode, int inputNum){ //Essentially SEARCH() with a removing function
+  bool numsRemoved = false;
+  while(numsRemoved == false){
+
+    
+  if(currNode->returnRight()->returnValue() == inputNum){ //Matching Case on Right Side | Remove & Resort
+    if(currNode->returnRight()->rightRight() == NULL && currNode->returnRight()->returnLeft() == NULL){ //No children case
+      delete currNode->returnRight();
+      currNode->makeRight(NULL);
+    }
+    else if(currNode->returnRight()->returnRight() == NULL && currNode->returnRight()->returnLeft() != NULL){//One child case (Var 1)
+      Node* tempNode = new Node();
+      tempNode = currNode;
+      currNode->makeRight(currNode->returnRight()->returnLeft());
+      delete tempNode->returnRight();
+    }
+    else if(currNode->returnRight()->returnRight() != NULL && currNode->returnRight()->returnLeft() == NULL){//One child case (Var 2)
+      Node* tempNode = new Node();
+      tempNode = currNode;
+      currNode->makeRight(currNode->returnRight()->returnRight());
+      delete tempNode->returnRight();
+    }
+    else if(currNode->returnRight()->returnRight() != NULL && currNode->returnRight()->returnLeft() != NULL){ //Two children case
+      bool nullFound = false;
+      Node* tempNode = new Node();
+      tempNode = currNode->returnRight()->returnRight();
+      Node* tempReplacementNode = new Node();
+      while(nullFound == false){ //Iterate until reach a left most NULL | temp node becomes the parent the left most non-Null Node
+	if(tempNode->returnLeft()->returnLeft() == NULL){ //How to delete the left most non-Null?
+	  nullFound = true;
+	}
+	else(){
+	    tempNode = tempNode->returnLeft;
+	  }
+      }
+
+      //Reordering/Restructuring tree | Replacing deleted position with node of right once left all the way
+      
+      tempReplacementNode = tempNode->returnLeft();
+      tempReplacementNode->makeRight(currNode->returnRight()->returnRight());
+      tempReplacementNode->makeLeft(currNode->returnRight()->returnLeft());
+      tempNode->makeLeft(NULL);
+      delete currNode->returnRight();
+      currNode->makeRight(tempReplacementNode);
+      
+    }
+
+  }
+  
+  } //End of while loop
 }
