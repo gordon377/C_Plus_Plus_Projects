0e3ce1e0f558c6a486dabb4677578ef86f60f536
U-DESKTOP-2GEDOLC\gjche <gjchen2005@gmail.com>
2024-06-03 17:34:58 -0700

    Overall main structure outlined

--- Graph_Creator/main.cpp
+++ Graph_Creator/main.cpp
@@ -23,5 +23,41 @@ using namespace std;
 
 
 int main(){ //main function
-  
+  vtex **grid = new vtex*[20];
+  int indexCount = 0;
+  bool active = true;
+  char input[20];
+  while(active){
+    cout << "What would you like to do? COMMANDS: PRINT, FASTESTPATH, ADDEDGE, DELEDGE, ADDVERTEX, DELVERTEX, or QUIT?" << endl;
+    cin >> input;
+    cin.clear();
+    cin.ignore(999, '\n');
+    if(strcmp(input, "PRINT") == 0){
+      PRINT(grid, indexCount);
+    }
+    else if(strcmp(input, "FASTESTPATH") == 0){
+      dijkstra(grid, indexCount);
+    }
+    else if(strcmp(input, "ADDEDGE") == 0){
+      addEdge(grid, indexCount);
+    }
+    else if(strcmp(input, "DELEDGE") == 0){
+      delEdge(grid, indexCount);
+    }
+    else if(strcmp(input, "ADDVERTEX") == 0){
+      addVtex(grid, indexCount);
+      indexCount++;
+    }
+    else if(strcmp(input, "DELVERTEX") == 0){
+      delVtex(grid, indexCount);
+      indexCount--;
+    }
+    else if(strcmp(input, "QUIT") == 0){
+      cout << "Program Quit" << endl;
+      active = false;
+    }
+    else{
+      cout << "Invalid Input" << endl;
+    }
+  }
 }
