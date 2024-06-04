86b8f40134f8a0d1114dce3cfc0392497a871196
U-DESKTOP-2GEDOLC\gjche <gjchen2005@gmail.com>
2024-06-03 18:09:43 -0700

    Search drafted, working on print

--- Graph_Creator/main.cpp
+++ Graph_Creator/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 //Function Prototypes
 
 vtex* search(vtex** grid, char input, int indexCount); //Finds & returns pointer to requested Vertex
-void print(vtex** grid, int indexCount); //Prints
+void print(vtex** grid, int indexCount); //Prints adjacency matrix
 void dijkstra(vtex** grid, int indexCount); //Uses Dijkstra's algorithm to find the shortest/fastest/most efficient path
 void addEdge(vtex** grid, int indexCount); //Adds Edge
 void delEdge(vtex** grid, int indexCount); //Deletes Edge
@@ -66,5 +66,70 @@ int main(){ //main function
 }
 
 vtex* search(vtex** grid, char input, int indexCount){
+  int a = 0;
+  while(a < indexCount){
+    if(grid[a]->getName() == input){
+      return grid[a];
+    }
+    else{
+      a++;
+    }
+  }
+  return NULL; //DNE
+}
+
+void print(vtex** gird, int indexCount){
+  char matrix[21][21];
+  for(int row = 0; row < 21; row++){    
+    for(int column = 0; column < 21; column++){
+      matrix[row][column] = ' '; //Empty Matrix
+    }
+  }
   
+  //Pre-Printing Processing
+  
+  int edgeMatrix[20][20];
+  int a = 0;
+  while(a < indexCount){
+    matrix[0][a+1] = grid[a]->returnName();
+    matrix[a+1][0] = grid[a]->returnName();
+    a++;
+  }
+  
+
+  //Edge Output List
+  for(int row = 0; row < indexCount; row++){
+    vtex* tempR = grid[row];
+    for(int column = 0; column < indexCount; column++){
+      vtex* tempC = grid[column];
+      if(tempR->returnEdge(tempC->returnIndex()) != 0){
+	edgeMatrix[row][column] = 'T'; //True
+      }
+      else{
+	edgeMatrix[row][column] = 'F'; //False
+      }
+    }
+  }
+  
+  //edgeMatrix -> matrix
+  for(int row = 0; row < indexCount; row++){
+    for(int column = 0; column < indexCount; column++){
+      matrix[row+1][column+1] = edgeMatrix[row][column];
+    }
+  }
+  
+  //PRINT ADJACENCY MATRIX
+  for(int row = 0; row < 21; row++){
+    for(int column = 0; column < 21; column++){
+      if(column != 0 && row != 0 && matrix[row][column] == 'T'){
+	cout << BLUE << matrix[row][column] << RESET << '\t';
+      }
+      else if(column != 0 && row != 0 && matrix[row][column] == 'F'){
+	cout << RED << matrix[row][column] << RESET << '\t';
+      }
+      else{
+	cout << matrix[row][column] << '\t';
+      }
+    }
+  }
 }
